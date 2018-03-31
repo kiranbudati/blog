822e80bc29c59ca2c8022ce080159eb55f42bd09
kiranbudati <kiranreddybudati503@gmail.com>
2018-03-31 20:09:54 +0530

    'hi'

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/client/dist/'));
+app.use(express.static(__dirname + './client/dist/'));
 app.use('/authentication',authentication);
 
 app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+    res.sendFile(path.join(__dirname + './client/dist/index.html'));
 });
 
 app.listen( process.env.PORT || 3000,() => {
